676b5a0307ec101438d5c1d7a487429a463359e0
Jake <Jakepeters199@hotmail.com>
2019-03-06 23:33:18 +0000

    Added slider for round timer

---
+++ Categories/Categories/Assets/Scripts/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    Text currentTimeDisplay;
+    public int currentRoundTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeDisplay = GetComponent<Text>();
+        currentRoundTime = 10;
+        currentTimeDisplay.text = currentRoundTime + "s";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void timeDisplayUpdater(float newTime)
+    {
+        currentTimeDisplay.text = newTime + "s";
+        //currentRoundTime = Mathf.RoundToInt(newTime);
+    }
+}
