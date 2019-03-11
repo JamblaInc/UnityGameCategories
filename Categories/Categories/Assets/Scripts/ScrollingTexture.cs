c36448d2b394e4f5012476005204218f3990a34b
Jake <Jakepeters199@hotmail.com>
2019-03-11 23:18:17 +0000

    ReSkin + ideas.txt

---
+++ Categories/Categories/Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingTexture : MonoBehaviour
+{
+    public float ScrollX = 0.0f;
+    public float ScrollY = -0.1f;
+    public Renderer _renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offsetX = Time.time * ScrollX;
+        float offsetY = Time.time * ScrollY;
+        _renderer.material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
