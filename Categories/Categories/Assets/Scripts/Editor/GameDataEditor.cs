b3d207e7859c25d48d86a058363a9fc8885c91ae
JamblaInc <Jakepeters199@hotmail.com>
2019-03-11 14:51:49 +0000

    added more questions

--- Categories/Categories/Assets/Scripts/Editor/GameDataEditor.cs
+++ Categories/Categories/Assets/Scripts/Editor/GameDataEditor.cs
@@ -7,8 +7,7 @@ using System.IO;
 public class GameDataEditor : EditorWindow
 {
 	public GameData gameData;
-
-	private string gameDataProjectFilePath = "/StreamingAssets/data.json";
+    private string gameDataProjectFilePath = "/StreamingAssets/data.json";
 
 	[MenuItem ("Window/Game Data Editor")]
 	static void Init()
@@ -19,26 +18,28 @@ public class GameDataEditor : EditorWindow
 
 	void OnGUI()
 	{
-		if (gameData != null) 
+        if (GUILayout.Button("Load data"))
+        {
+            LoadGameData();
+        }
+
+        if (gameData != null) 
 		{
-			SerializedObject serializedObject = new SerializedObject (this);
+            if (GUILayout.Button("Save data"))
+            {
+                SaveGameData();
+            }
+
+            SerializedObject serializedObject = new SerializedObject (this);
 			SerializedProperty serializedProperty = serializedObject.FindProperty("gameData");
 
 			EditorGUILayout.PropertyField (serializedProperty, true);
 
 			serializedObject.ApplyModifiedProperties ();
 
-			if (GUILayout.Button ("Save data")) 
-			{
-				SaveGameData ();
-			}
+			
 		}
-
-		if (GUILayout.Button ("Load data")) 
-		{
-			LoadGameData ();
-		}
-	}
+    }
 
 	private void LoadGameData()
 	{
