5358b883e495e28f76b96c3ec4e054e52be6edaa
Jake <Jakepeters199@hotmail.com>
2019-03-20 00:36:28 +0000

    BannerAds config

--- Categories/Categories/Assets/Scripts/AdManager.cs
+++ Categories/Categories/Assets/Scripts/AdManager.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
+using GoogleMobileAds.Api;
+
 
 public class AdManager : MonoBehaviour
 {
+    private BannerView bannerView;
+    [SerializeField] private string appID = "ca-app-pub-2714254015915544~1740214392";
+    [SerializeField] private string bannerID = "ca-app-pub-3940256099942544/6300978111";
 
-    // Update is called once per frame
-    void Update()
+    void start()
     {
-        
+        MobileAds.Initialize(appID);
+        RequestBanner();
+    }
+
+    private void RequestBanner()
+    {
+        bannerView = new BannerView(bannerID, AdSize.Banner, AdPosition.Bottom);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+
+    }
+
+    public void OnClickShowBanner()
+    {
+        this.RequestBanner();
     }
 
     public void playAd()
